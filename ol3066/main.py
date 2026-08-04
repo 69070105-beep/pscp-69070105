7455bb4f011eabaa3867320c10d7973c280f92a6
69070105-beep <69070105@kmitl.ac.th>
2026-08-05 03:26:44 +0700

    week 2

---
+++ ol3066/main.py
@@ -0,0 +1,12 @@
+"""เหมือนกันหมด"""
+
+num1 = input()
+num2 = input()
+num3 = input()
+
+if num1 == num2 == num3:
+    print("all the same")
+elif num1 != num2 != num3 != num1:
+    print("all different")
+else:
+    print("neither")
